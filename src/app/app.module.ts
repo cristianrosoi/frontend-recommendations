8595ea6e6c9286bc08f9b9c188da7422feccaf78
Cristian Rosoi <cristian.rosoi@gmail.com>
2020-02-27 22:07:20 +0200

    added basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecommendationsComponent } from './modules/pages/recommendations/recommendations.component';
+import { HeaderComponent } from './core/components/header/header.component';
+import { LoginComponent } from './core/auth/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecommendationsComponent,
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
